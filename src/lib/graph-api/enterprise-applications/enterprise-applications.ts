32642d9db36f3b68cbdf0c91055df04db227b4fd
Justin <justin.mcmahon@pliancy.comrything.com>
2026-02-23 12:53:42 -0800

    feat(graph.enterprise-applications): add Graph enterprise-applications module

---
+++ src/lib/graph-api/enterprise-applications/enterprise-applications.ts
@@ -0,0 +1,56 @@
+import { AxiosInstance } from 'axios'
+import { AppRoleAssignment, ServicePrincipal } from './enterprise-applications.types'
+
+export class EnterpriseApplications {
+    constructor(private readonly http: AxiosInstance) {}
+
+    /**
+     * Retrieves a ServicePrincipal object by its application ID.
+     *
+     * @param {string} appId - The application ID of the ServicePrincipal to retrieve.
+     * @return {Promise<ServicePrincipal>} A promise that resolves to the ServicePrincipal object associated with the given appId. Returns null if no matching ServicePrincipal is found.
+     */
+    async getByAppId(appId: string): Promise<ServicePrincipal> {
+        const { data: servicePrincipal } = await this.http.get(
+            `servicePrincipals?$filter=appId eq '${appId}'`,
+        )
+        return servicePrincipal?.value?.[0] ?? null
+    }
+
+    /**
+     * Retrieves the list of app role assignments for a specified principal.
+     *
+     * @param {string} principalId - The unique identifier of the principal (service principal or user) for which app role assignments are being retrieved.
+     * @return {Promise<AppRoleAssignment[]>} A promise that resolves to an array of app role assignments associated with the specified principal.
+     */
+    async getAppRoleAssignments(principalId: string): Promise<AppRoleAssignment[]> {
+        const { data: appRoleAssignments } = await this.http.get(
+            `servicePrincipals/${principalId}/appRoleAssignments`,
+        )
+        return appRoleAssignments.value ?? []
+    }
+
+    /**
+     * Assigns an app role to a specified service principal.
+     *
+     * @param {string} principalId - The unique identifier of the service principal to which the role assignment will be granted.
+     * @param {string} resourceId - The identifier of the resource (service principal) that owns the app role being assigned.
+     * @param {string} appRoleId - The unique identifier of the app role being assigned.
+     * @return {Promise<AppRoleAssignment>} A promise that resolves to an AppRoleAssignment object containing the details of the assignment.
+     */
+    async grantAppRoleAssignment(
+        principalId: string,
+        resourceId: string,
+        appRoleId: string,
+    ): Promise<AppRoleAssignment> {
+        const { data: appRoleAssignment } = await this.http.post(
+            `servicePrincipals/${principalId}/appRoleAssignments`,
+            {
+                principalId,
+                resourceId,
+                appRoleId,
+            },
+        )
+        return appRoleAssignment
+    }
+}
---
+++ src/lib/graph-api/enterprise-applications/enterprise-applications.types.ts
@@ -0,0 +1,71 @@
+export interface ServicePrincipal {
+    id: string // The unique UUID for the service principal object in the directory
+    appId: string // The unique identifier for the associated application
+    displayName: string
+    active: boolean
+
+    // Identifiers
+    appDisplayName?: string
+    appDescription?: string
+    servicePrincipalNames: string[]
+    replyUrls: string[]
+
+    // Enterprise App Specifics
+    accountEnabled: boolean
+    alternativeNames: string[]
+    appRoleAssignmentRequired: boolean
+    servicePrincipalType: 'Application' | 'ManagedIdentity' | 'Legacy'
+    tags: string[] // Often used for "WindowsAzureActiveDirectoryCustomSingleSignOnApplication"
+
+    // Permissions & Roles
+    appRoles: AppRole[]
+    oauth2PermissionScopes: PermissionScope[]
+
+    // Metadata
+    createdDateTime?: string
+    description?: string
+    homepage?: string
+    logoutUrl?: string
+    notes?: string
+
+    // Owners and Policy
+    preferredSingleSignOnMode?: 'saml' | 'password' | 'oath' | 'none'
+    preferredTokenSigningKeyThumbprint?: string
+    samlSingleSignOnSettings?: SamlSingleSignOnSettings
+
+    // Reference to the App in the tenant
+    info?: {
+        logoUrl?: string
+        marketingUrl?: string
+        privacyStatementUrl?: string
+        supportUrl?: string
+        termsOfServiceUrl?: string
+    }
+}
+
+export interface AppRole {
+    id: string
+    allowedMemberTypes: ('User' | 'Application')[]
+    description: string
+    displayName: string
+    isEnabled: boolean
+    value: string
+}
+
+export interface PermissionScope {
+    id: string
+    adminConsentDescription: string
+    adminConsentDisplayName: string
+    isEnabled: boolean
+    type: 'User' | 'Admin'
+    value: string
+}
+
+export interface SamlSingleSignOnSettings {
+    baseUrl?: string
+}
+
+export interface AppRoleAssignment {
+    appRoleId: string
+    resourceId: string
+}
