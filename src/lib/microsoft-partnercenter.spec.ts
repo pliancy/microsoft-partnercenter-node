7930cd91e057e9c5c124f301e416f81a605772bc
Justin <justin.mcmahon@pliancy.comrything.com>
2026-04-10 14:55:27 -0700

    fix(partnercenter): fix accept header

--- src/lib/microsoft-partnercenter.spec.ts
+++ src/lib/microsoft-partnercenter.spec.ts
@@ -358,7 +358,7 @@ describe('Microsoft Partner Center', () => {
                 method: 'get',
                 url: 'https://download.microsoft.com/download/e/3/e/e3e9faf2-f28b-490a-9ada-c6089a1fc5b0/Product%20names%20and%20service%20plan%20identifiers%20for%20licensing.csv',
                 headers: {
-                    'Accept-Encoding': 'text/csv',
+                    Accept: 'text/csv',
                 },
             })
             expect(result).toEqual([{ Product: 'Prod1', ServicePlan: 'plan-a' }])
--- src/lib/microsoft-partnercenter.ts
+++ src/lib/microsoft-partnercenter.ts
@@ -407,7 +407,7 @@ export class MicrosoftPartnerCenter extends MicrosoftApiBase {
             method: 'get',
             url: 'https://download.microsoft.com/download/e/3/e/e3e9faf2-f28b-490a-9ada-c6089a1fc5b0/Product%20names%20and%20service%20plan%20identifiers%20for%20licensing.csv',
             headers: {
-                'Accept-Encoding': 'text/csv',
+                Accept: 'text/csv',
             },
         })
 
